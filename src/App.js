1565a636e79531a20f4574eb01bb3cf758dc594d
Eric J <ejuquel@yahoo.fr>
2021-03-20 20:13:36 +0100

    fix burger conditional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.scss";
-import Burger from "./components/Burger";
-import Languages from "./components/Languages";
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
-import SocialNetworks from "./components/SocialNetworks";
-import Home from "./components/Home";
-import Skills from "./components/Skills";
-import Achievements from "./components/Achievements";
-import Career from "./components/Career";
-import Contact from "./components/Contact";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.scss';
+import Burger from './components/Burger';
+import Languages from './components/Languages';
+import Logo from './components/Logo';
+import Navigation from './components/Navigation';
+import SocialNetworks from './components/SocialNetworks';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Achievements from './components/Achievements';
+import Career from './components/Career';
+import Contact from './components/Contact';
 
-import { Container, useMediaQuery } from "@material-ui/core";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { orange, teal, red, blue } from "@material-ui/core/colors";
+import { Container, useMediaQuery } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { orange, teal, red, blue } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
-    type: "dark",
+    type: 'dark',
     primary: {
       main: orange[300],
     },
@@ -27,32 +27,32 @@ const theme = createMuiTheme({
     },
     info: {
       main: blue[500],
-      dark: "rgb(33, 150, 243,0.4)",
+      dark: 'rgb(33, 150, 243,0.4)',
     },
     error: {
       main: red[500],
-      dark: "rgba(211, 47, 47, 0.4)",
+      dark: 'rgba(211, 47, 47, 0.4)',
     },
     warning: {
-      main: "rgb(229, 0, 113)",
-      dark: "rgba(229, 0, 113,0.4)",
-      light: "rgb(255, 64, 159);",
+      main: 'rgb(229, 0, 113)',
+      dark: 'rgba(229, 0, 113,0.4)',
+      light: 'rgb(255, 64, 159);',
     },
     text: {
-      primary: "rgba(255, 255, 255,0.87)",
-      secondary: "rgba(255, 255, 255, 0.6)",
-      disabled: "rgba(255, 255, 255,0.38)",
-      hint: "#1c2430",
+      primary: 'rgba(255, 255, 255,0.87)',
+      secondary: 'rgba(255, 255, 255, 0.6)',
+      disabled: 'rgba(255, 255, 255,0.38)',
+      hint: '#1c2430',
     },
     background: {
-      paper: "#1c2430",
+      paper: '#1c2430',
     },
   },
 
   typography: {
     h1: {
       fontFamily: `"Montserrat","Roboto","Helvetica"`,
-      textTransform: "capitalize",
+      textTransform: 'capitalize',
       fontSize: 70,
       marginBottom: 50,
       '@media (max-width:600px)': {
@@ -64,7 +64,7 @@ const theme = createMuiTheme({
     },
     h4: {
       fontFamily: `"Montserrat","Roboto","Helvetica"`,
-      textTransform: "capitalize",
+      textTransform: 'capitalize',
     },
     h5: {
       fontSize: 25,
@@ -82,9 +82,8 @@ const theme = createMuiTheme({
       '@media (max-width:600px)': {
         fontSize: 20,
       },
-      fontWeight: "100",
-      color: "rgba(255, 255, 255, 0.6)",
-
+      fontWeight: '100',
+      color: 'rgba(255, 255, 255, 0.6)',
     },
 
     body1: {
@@ -98,7 +97,7 @@ const theme = createMuiTheme({
 });
 
 const App = () => {
-  const matches = useMediaQuery("(max-width:960px)");
+  const matches = useMediaQuery('(max-width:960px)');
 
   return (
     <Router>
@@ -107,12 +106,15 @@ const App = () => {
           <div className="languageBtn">
             <Languages />
           </div>
-          <header className="side-menu">
-            <Logo />
-            <Navigation />
-            <SocialNetworks />
-          </header>
-          {matches && <Burger />}
+          {!matches ? (
+            <header className="side-menu">
+              <Logo />
+              <Navigation />
+              <SocialNetworks />
+            </header>
+          ) : (
+            <Burger />
+          )}
           <Container maxWidth={false} component="main">
             <Switch>
               <Route exact path="/" component={Home} />
