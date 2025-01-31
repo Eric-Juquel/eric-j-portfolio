546df05bfb1c4e3fd0c512432bc75d049c8664b9
Eric Juquel <eric.juquel@ratp.fr>
2025-01-31 16:20:29 +0100

    new deployment

--- .history/src/App_20250131161347.js
+++ .history/src/App_20250131161347.js
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.scss";
-import Burger from "./components/navigation/Burger";
-import Languages from "./components/Languages";
-import Logo from "./components/navigation/Logo";
-import Navigation from "./components/navigation/Navigation";
-import SocialNetworks from "./components/navigation/SocialNetworks";
-import Home from "./components/Home";
-import Skills from "./components/skills/Skills";
-import Achievements from "./components/achievments/Achievements";
-import Career from "./components/Career";
-import Contact from "./components/contact/Contact";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.scss';
+import Burger from './components/navigation/Burger';
+import Languages from './components/Languages';
+import Logo from './components/navigation/Logo';
+import Navigation from './components/navigation/Navigation';
+import SocialNetworks from './components/navigation/SocialNetworks';
+import Home from './components/Home';
+import Skills from './components/skills/Skills';
+import Achievements from './components/achievments/Achievements';
+import Career from './components/Career';
+import Contact from './components/contact/Contact';
 
-import { Container, useMediaQuery } from "@material-ui/core";
-import { ThemeProvider, createTheme } from "@material-ui/core/styles";
-import { orange, teal, red, blue } from "@material-ui/core/colors";
+import { Container, useMediaQuery } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { orange, teal, red, blue } from '@material-ui/core/colors';
 
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
-    type: "dark",
+    type: 'dark',
     primary: {
       main: orange[300],
     },
@@ -26,70 +26,70 @@ const theme = createTheme({
     },
     info: {
       main: blue[500],
-      dark: "rgb(33, 150, 243,0.4)",
+      dark: 'rgb(33, 150, 243,0.4)',
     },
     error: {
       main: red[500],
-      dark: "rgba(211, 47, 47, 0.4)",
+      dark: 'rgba(211, 47, 47, 0.4)',
     },
     warning: {
-      main: "rgb(229, 0, 113)",
-      dark: "rgba(229, 0, 113,0.4)",
-      light: "rgb(255, 64, 159);",
+      main: 'rgb(229, 0, 113)',
+      dark: 'rgba(229, 0, 113,0.4)',
+      light: 'rgb(255, 64, 159);',
     },
     text: {
-      primary: "rgba(255, 255, 255,0.90)",
-      secondary: "rgba(255, 255, 255, 0.70)",
-      disabled: "rgba(255, 255, 255,0.38)",
-      hint: "#1c2430",
+      primary: 'rgba(255, 255, 255,0.90)',
+      secondary: 'rgba(255, 255, 255, 0.70)',
+      disabled: 'rgba(255, 255, 255,0.38)',
+      hint: '#1c2430',
     },
     background: {
-      paper: "#1c2430",
+      paper: '#1c2430',
     },
   },
 
   typography: {
     h1: {
       fontFamily: `"Montserrat","Roboto","Helvetica"`,
-      textTransform: "capitalize",
+      textTransform: 'capitalize',
       fontSize: 70,
       marginBottom: 50,
-      "@media (max-width:600px)": {
+      '@media (max-width:600px)': {
         fontSize: 50,
       },
-      "@media (max-width:440px)": {
+      '@media (max-width:440px)': {
         fontSize: 40,
       },
     },
     h4: {
       fontFamily: `"Montserrat","Roboto","Helvetica"`,
       fontWeight: 600,
-      textTransform: "capitalize",
+      textTransform: 'capitalize',
     },
     h5: {
       fontSize: 25,
       fontWeight: 400,
       letterSpacing: 1.5,
-      "@media (max-width:600px)": {
+      '@media (max-width:600px)': {
         fontSize: 23,
       },
-      "@media (max-width:440px)": {
+      '@media (max-width:440px)': {
         fontSize: 20,
       },
     },
     h6: {
       fontSize: 23,
-      "@media (max-width:600px)": {
+      '@media (max-width:600px)': {
         fontSize: 20,
       },
-      fontWeight: "300",
-      color: "rgba(255, 255, 255, 0.6)",
+      fontWeight: '300',
+      color: 'rgba(255, 255, 255, 0.6)',
     },
 
     body1: {
       fontSize: 20,
       fontWeight: 400,
-      "@media (max-width:600px)": {
+      '@media (max-width:600px)': {
         fontSize: 18,
       },
     },
@@ -100,7 +100,7 @@ const theme = createTheme({
 });
 
 const App = () => {
-  const matches = useMediaQuery("(max-width:961px)");
+  const matches = useMediaQuery('(max-width:961px)');
 
   return (
     <Router>
